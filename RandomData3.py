19e4fedb7e2a204f32c78078b1c23df92f2219d9
Blas-U <cheezypizzagamer@gmail.com>
2023-05-01 19:56:28 -0500

    Update

--- RandomData3.py
+++ RandomData3.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 #read file, open data.
-with open("Random3Data.csv") as file:
+with open("SMA-7_for_Blas.csv", encoding='utf-8-sig') as file:
     data = file.readlines()
+    print(data)
     for i in range(len(data)):
-        data[i] = float(data[i]) # change from string to floating point numbers.
+        data[i] = float(data[i].strip()) # change from string to floating point numbers.
         #print(i,data[i])
 numdata = len(data)
 popavg = np.average(data)
@@ -13,30 +14,39 @@ print("popavg =",popavg)
 print("popstdev =",popstdev)
 print("numdata =", numdata)
 
+
+#peramiters
+Fraction = .50 #% of data
+nTimes = 10 #number of times subsampled
+
+
+
 #test with 10 data points.
-for i in range(10):
-    n = int(numdata*.50)
+for i in range(nTimes):
+    n = int(numdata*Fraction)
     d = np.random.choice(data, n)
-    #print(d)
-    #print(n)
+    print()
+    print(f'Iteration: {i +1}')
+    print(d)
+    print(n)
     sampleAvg = np.average(d)
     samplestdev = np.std(d)
-    print(f"{n}, {sampleAvg}, {samplestdev}")
+    print(f"Number of Samples: {n}, Average: {sampleAvg}, Standard Deviation: {samplestdev}")
 
-for i in range(10):
-    n = int(numdata*.75)
-    d = np.random.choice(data, n)
-    #print(d)
-    #print(n)
-    sampleAvg = np.average(d)
-    samplestdev = np.std(d)
-    print(f"{n}, {sampleAvg}, {samplestdev}")
+# for i in range(10):
+#     n = int(numdata*.75)
+#     d = np.random.choice(data, n)
+#     #print(d)
+#     #print(n)
+#     sampleAvg = np.average(d)
+#     samplestdev = np.std(d)
+#     print(f"{n}, {sampleAvg}, {samplestdev}")
 
-for i in range(1):
-    n = int(numdata*1)
-    d = np.random.choice(data, n)
-    #print(d)
-    #print(n)
-    sampleAvg = np.average(d)
-    samplestdev = np.std(d)
-    print(f"{n}, {sampleAvg}, {samplestdev}")
+# for i in range(1):
+#     n = int(numdata*1)
+#     d = np.random.choice(data, n)
+#     #print(d)
+#     #print(n)
+#     sampleAvg = np.average(d)
+#     samplestdev = np.std(d)
+#     print(f"{n}, {sampleAvg}, {samplestdev}")
